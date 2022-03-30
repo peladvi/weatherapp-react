f369993ba2f823a4057788602373e8578fc0ba0f
peladvi <99794434+peladvi@users.noreply.github.com>
2022-03-30 10:39:33 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,23 @@ function App() {
       <div className="App">
         <Weather />
       </div>
+      <footer>
+        <a
+          href="https://github.com/peladvi/weatherapp-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code,
+        </a>
+        <span> by Paula Ladwiniec | weather icons from </span>
+        <a
+          href="https://erikflowers.github.io/weather-icons/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          here
+        </a>
+      </footer>
     </div>
   );
 }
