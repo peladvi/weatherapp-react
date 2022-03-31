a2202f500b23ffc411faeb72ecaa08124c7b7299
peladvi <99794434+peladvi@users.noreply.github.com>
2022-03-31 16:36:48 +0200

    Moved search.js and current.js into weather.js

--- src/App.js
+++ src/App.js
@@ -4,27 +4,27 @@ import Weather from "./Weather.js";
 
 function App() {
   return (
-    <div class="container">
+    <div className="container">
       <div className="App">
         <Weather />
+        <footer>
+          <a
+            href="https://github.com/peladvi/weatherapp-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code,
+          </a>
+          <span> by Paula Ladwiniec | weather icons from </span>
+          <a
+            href="https://erikflowers.github.io/weather-icons/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            here
+          </a>
+        </footer>
       </div>
-      <footer>
-        <a
-          href="https://github.com/peladvi/weatherapp-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code,
-        </a>
-        <span> by Paula Ladwiniec | weather icons from </span>
-        <a
-          href="https://erikflowers.github.io/weather-icons/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          here
-        </a>
-      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,38 @@
 import React from "react";
-import Search from "./Search";
 import "./App.css";
-import Current from "./Current";
+import "./weather.css";
 import Icon from "./Icon";
 
 export default function Weather() {
   return (
-    <section>
+    <section className="Weather">
       <h1>What's your weather?</h1>
-      <Search />
+
+      <form className="Search">
+        <input
+          type="text"
+          placeholder="Enter a city"
+          className="search-input"
+          id="search-input"
+          autocomplete="off"
+          autofocus="on"
+        />
+        <input type="submit" value="Search" className="button" />
+        <button className="button">Current location</button>
+      </form>
+
       <div class="row">
-        <Current />
+        <div className="col-6">
+          <h2 className="city">Naples</h2>
+          <div className="today">Tuesday 11:05</div>
+          <h2>
+            <span className="temperature">15</span>
+            <span className="units">°C</span>
+          </h2>
+          <strong className="description">Clear Sky</strong>
+          <div className="humidity"> Humidity: 60%</div>
+          <div className="wind-speed">Wind Speed: 5m/s</div>
+        </div>
         <Icon />
       </div>
     </section>
